f4f76bd2aee8d54f31afb00582602c56b00186c4
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-01-02 17:33:05 +0530

    Create SortedRotated.java

---
+++ Day XX /SortedRotated.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class SortedRotated {
+
+    public static boolean pair2(ArrayList<Integer> list, int target) {
+        int bp = -1;
+        int n = list.size();
+
+        for (int i = 0; i < n; i++) {
+            if (list.get(i) > list.get((i + 1) % n)) {
+                bp = i;
+                break;
+            }
+        }
+
+        int lp = bp + 1;  // smallest
+        int rp = bp;  // largest
+
+        while (rp != lp) {
+            // case 1
+            if (list.get(lp) + list.get(rp) == target) {
+                return true;
+            }
+            // case 2
+            if (list.get(lp) + list.get(rp) < target) {
+                lp = (lp + 1) % n;
+            } else {
+                rp = (n + rp - 1) % n;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+
+        int target = 16;
+        System.out.println(list);
+        System.out.println(pair2(list, target));
+    }
+}
